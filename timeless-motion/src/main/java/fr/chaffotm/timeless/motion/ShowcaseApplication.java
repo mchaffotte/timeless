b261a84f351e59ad0582f134a1ef7032168ea689
Matthieu Chaffotte <matthieu.chaffotte@gmail.com>
2020-02-05 22:30:27 +0100

    Initialize project

---
+++ timeless-motion/src/main/java/fr/chaffotm/timeless/motion/ShowcaseApplication.java
@@ -0,0 +1,14 @@
+package fr.chaffotm.timeless.motion;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ShowcaseApplication  extends Application {
+
+    @Override
+    public void start(final Stage primaryStage) {
+        primaryStage.setTitle("Timeless - Motion");
+        primaryStage.show();
+    }
+
+}
